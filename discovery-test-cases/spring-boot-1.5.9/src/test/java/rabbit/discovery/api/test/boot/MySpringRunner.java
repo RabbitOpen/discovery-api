26a5e90ad7a93a666810362db729fa3cf352ff51
RabbitOpen <709498191@qq.com>
2023-11-09 15:23:39 +0800

    init

--- discovery-test-cases/spring-boot-1.5.9/src/test/java/rabbit/discovery/api/test/boot/MySpringRunner.java
+++ discovery-test-cases/spring-boot-1.5.9/src/test/java/rabbit/discovery/api/test/boot/MySpringRunner.java
@@ -7,7 +7,6 @@ import rabbit.discovery.api.common.rpc.ConfigService;
 import rabbit.discovery.api.test.service.ConfigServiceImpl;
 import rabbit.discovery.api.test.service.DiscoveryServiceImpl;
 import rabbit.discovery.api.test.spi.ApiCache;
-import rabbit.flt.common.Metrics;
 import rabbit.flt.rpc.common.ServerNode;
 import rabbit.flt.rpc.common.rpc.ProtocolService;
 import rabbit.flt.rpc.server.Server;
@@ -15,7 +14,6 @@ import rabbit.flt.rpc.server.ServerBuilder;
 
 import java.net.StandardSocketOptions;
 import java.util.Arrays;
-import java.util.List;
 
 public class MySpringRunner extends SpringJUnit4ClassRunner {
 
@@ -28,17 +26,7 @@ public class MySpringRunner extends SpringJUnit4ClassRunner {
                 .host("localhost").port(port)
                 .socketOption(StandardSocketOptions.SO_RCVBUF, 256 * 1024)
                 .socketOption(StandardSocketOptions.SO_REUSEADDR, true)
-                .registerHandler(ProtocolService.class, new ProtocolService() {
-                    @Override
-                    public List<ServerNode> getServerNodes() {
-                        return Arrays.asList(new ServerNode("localhost", port));
-                    }
-
-                    @Override
-                    public boolean isMetricsEnabled(String applicationCode, Class<? extends Metrics> type) {
-                        return false;
-                    }
-                })
+                .registerHandler(ProtocolService.class, () -> Arrays.asList(new ServerNode("localhost", port)))
                 .registerHandler(rabbit.discovery.api.common.rpc.ProtocolService.class, DiscoveryServiceImpl.getInstance())
                 .registerHandler(ConfigService.class, ConfigServiceImpl.getInstance())
                 .registerHandler(ApiReportService.class, (application, className, apiList) -> ApiCache.getMap().put(className, apiList))
