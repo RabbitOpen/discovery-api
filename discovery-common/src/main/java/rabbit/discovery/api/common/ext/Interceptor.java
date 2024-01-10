59d3997267a6396233c355a7e8309d14bbbdfc4d
RabbitOpen <709498191@qq.com>
2024-01-10 11:04:55 +0800

    init

--- discovery-common/src/main/java/rabbit/discovery/api/common/ext/Interceptor.java
+++ discovery-common/src/main/java/rabbit/discovery/api/common/ext/Interceptor.java
@@ -5,7 +5,7 @@ public interface Interceptor {
     /**
      * 请求拦截
      * @param request
-     * @return  true, 业务系统自己接管，esg不再过问
+     * @return  true, 业务系统自己接管
      */
     boolean intercept(HttpRequest request);
 }
